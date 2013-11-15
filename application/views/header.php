df70e284c3c26ab4f90aa9d7acf2df1109a07236
dc-mani <manikandan@digitalchakra.in>
2013-11-15 18:32:08 +0530

    show self pages link only in top menu

--- application/views/header.php
+++ application/views/header.php
@@ -39,9 +39,9 @@ $subpage = $this->uri->segment(2); ?>
         	<li class="inner_logo"><a href="<?php echo base_url(); ?>">Samaras One Home</a></li>
         </ul>
         <ul class="menu_bg">
-        	<li class="commercial"><a href="<?php echo base_url('commercial'); ?>">Commercial</a></li>
-        	<li class="residential"><a href="<?php echo base_url('residential'); ?>">Residential</a></li>
-        	<li class="business"><a href="<?php echo base_url('business'); ?>">Business</a></li>
+        	<? if($page!='business') { ?><li class="commercial"><a href="<?php echo base_url('commercial'); ?>">Commercial</a></li>
+        	<li class="residential"><a href="<?php echo base_url('residential'); ?>">Residential</a></li><? } else {?>
+        	<li class="business"><a href="<?php echo base_url('business'); ?>">Business</a></li><? } ?>
         </ul>
       </div>
     	
